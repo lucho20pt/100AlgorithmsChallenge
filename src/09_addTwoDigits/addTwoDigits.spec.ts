673d7f434c58b51a88d0c6298246cbedc3deba8c
Daniel Batista <dani_batista20@hotmail.com>
2022-10-15 17:58:17 +0100

    add: 09_addTwoDigits

--- src/09_addTwoDigits/addTwoDigits.spec.ts
+++ src/09_addTwoDigits/addTwoDigits.spec.ts
@@ -1,14 +1,25 @@
-import { addTwoDigits } from './addTwoDigits';
+import { addTwoDigits } from './addTwoDigits'
 
-xdescribe(addTwoDigits.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const data = 29;
+describe(addTwoDigits.name, () => {
+  it('Test 1', () => {
+    // arrange
+    const data = 29
 
-        // act
-        const response = addTwoDigits(data);
+    // act
+    const response = addTwoDigits(data)
 
-        // assert
-        expect(response).toBe(11);
-    });
-});
+    // assert
+    expect(response).toBe(11)
+  })
+
+  it('Test 2', () => {
+    // arrange
+    const data = 99
+
+    // act
+    const response = addTwoDigits(data)
+
+    // assert
+    expect(response).toBe(18)
+  })
+})
--- src/09_addTwoDigits/addTwoDigits.ts
+++ src/09_addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 export function addTwoDigits(n: any): number {
-    
+  // num => toString => toStringsArray => toNumsArray
+  const numArr = n.toString().split('').map(Number)
+
+  return numArr.reduce((a: number, b: number) => a + b, 0)
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29))
+console.log(addTwoDigits(99))
