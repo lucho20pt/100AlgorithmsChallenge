7b6527aa6701b796ac24c8b6bdf76b3eb2e98924
Daniel Batista <dani_batista20@hotmail.com>
2022-10-15 18:17:25 +0100

    add: 10_adjacentElementsProduct

--- src/10_adjacentElementsProduct/adjacentElementsProduct.ts
+++ src/10_adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,6 +1,12 @@
-// My Solution
 export function adjacentElementsProduct(inputArray: number[]): number {
-    
+  let product = 0
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    let temp = inputArray[i] * inputArray[i + 1]
+    if (product < temp) {
+      product = temp
+    }
+  }
+  return product
 }
 
-// console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
