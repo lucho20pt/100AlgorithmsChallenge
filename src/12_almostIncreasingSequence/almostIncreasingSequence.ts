8e74491d11bdd0d5e256940304e93d9235e6930b
Daniel Batista <dani_batista20@hotmail.com>
2022-10-15 21:03:01 +0100

    add: 12_almostIncreasingSequence

--- src/12_almostIncreasingSequence/almostIncreasingSequence.ts
+++ src/12_almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,15 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
-
+  let count = 0
+  for (let i = 1; i < sequence.length; i++) {
+    let prev = sequence[i - 1]
+    let temp = sequence[i]
+    if (!(prev < temp) || !(count <= 1)) {
+      count++
+    }
+  }
+  return count <= 1
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]))
+console.log(almostIncreasingSequence([1, 3, 2]))
+console.log(almostIncreasingSequence([1, 5, 6, 4, 7]))
