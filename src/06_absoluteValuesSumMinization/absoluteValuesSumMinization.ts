0495eb3024014aee36d5b920dbd70c9f58570fce
Daniel Batista <dani_batista20@hotmail.com>
2022-10-15 17:08:57 +0100

    add: 06_absoluteValuesSumMinization

--- src/06_absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ src/06_absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,13 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    
+  const isEven = a.length % 2 === 0
+  const res = isEven
+    ? Math.abs(a[Math.floor(a.length / 2) - 1])
+    : Math.abs(a[Math.floor(a.length / 2)])
+
+  return res
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]))
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]))
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]))
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]))
