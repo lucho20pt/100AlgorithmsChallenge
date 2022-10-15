c969839a9fc59e9a7bb19264502111d15cf74e15
Daniel Batista <dani_batista20@hotmail.com>
2022-10-15 17:27:50 +0100

    add: 08_addBorder

--- src/08_addBorder/addBorder.ts
+++ src/08_addBorder/addBorder.ts
@@ -1,5 +1,11 @@
 export function addBorder(picture: string[]): string[] {
-
+  const border = '*'.repeat(picture[0].length + 2)
+  for (let i = 0; i < picture.length; i++) {
+    picture[i] = '*' + picture[i] + '*'
+  }
+  picture.unshift(border)
+  picture.push(border)
+  return picture
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']))
