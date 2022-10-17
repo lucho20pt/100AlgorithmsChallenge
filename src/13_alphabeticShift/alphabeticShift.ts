692951880b7638e0f74dc0ddeb9c54c05429b586
Daniel Batista <dani_batista20@hotmail.com>
2022-10-17 12:30:05 +0100

    add: 13_alphabeticShift

--- src/13_alphabeticShift/alphabeticShift.ts
+++ src/13_alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,10 @@
 export function alphabeticShift(inputString: string): string {
-
+  let newStr: string[] = []
+  for (let i = 0; i < inputString.length; i++) {
+    newStr.push(String.fromCharCode(inputString[i].charCodeAt(0) + 1))
+  }
+  let res: string = newStr.join('').replace(/{/g, 'a')
+  return res
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('crazy'))
