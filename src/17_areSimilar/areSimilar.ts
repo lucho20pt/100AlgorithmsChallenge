08aac5fe51d4832c3a91fb371e3d2fae4cf7216b
Daniel Batista <dani_batista20@hotmail.com>
2022-10-18 19:54:03 +0100

    add: 17_areSimilar

--- src/17_areSimilar/areSimilar.ts
+++ src/17_areSimilar/areSimilar.ts
@@ -1,7 +1,24 @@
 export function areSimilar(a: number[], b: number[]): boolean {
-
+  let count: number = 0
+  if (
+    a.toString() === b.toString() ||
+    a.toString() === b.reverse().toString()
+  ) {
+    return true
+  }
+  for (let i = 0; i < a.length; i++) {
+    let ai: number = a[i]
+    for (let j = 0; j < b.length; j++) {
+      let bj: number = b[j]
+      if (ai === bj) {
+        count++
+      }
+    }
+  }
+  return count === a.length && count === b.length
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]))
+console.log(areSimilar([1, 2, 3], [2, 1, 3]))
+console.log(areSimilar([1, 2, 3], [3, 2, 1]))
+console.log(areSimilar([1, 2, 2], [2, 1, 1]))
