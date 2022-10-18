52b7034bce7b8cdaac9d46370d96b4965bef3412
Daniel Batista <dani_batista20@hotmail.com>
2022-10-18 17:47:39 +0100

    add: 15_alternatingSums

--- src/15_alternatingSums/alternatingSums.ts
+++ src/15_alternatingSums/alternatingSums.ts
@@ -1,5 +1,19 @@
 export function alternatingSums(a: number[]): number[] {
+  let team1: number = 0
+  let team2: number = 0
+  const totalWeights: number[] = []
+  for (let i = 0; i < a.length; i++) {
+    let isEven = i % 2 === 0
+    if (isEven) {
+      team1 += a[i]
+    } else {
+      team2 += a[i]
+    }
+  }
+  totalWeights.push(team1)
+  totalWeights.push(team2)
 
+  return totalWeights
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
